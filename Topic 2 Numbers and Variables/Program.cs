2971789478feed86de7e960897114718ed1e7951
Jack Gallagher <339690067@GoTVDSB.ca>
2024-09-06 09:17:48 -0400

    Finished Tutorial

--- Topic 2 Numbers and Variables/Program.cs
+++ Topic 2 Numbers and Variables/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int a = 18;
+            double PI = Math.PI;
+
+            Console.WriteLine(a);
+            Console.WriteLine(18 / 3);
+            Console.WriteLine(19 / 3);
+            Console.WriteLine(23 / 3);
+            Console.WriteLine(24 / 3);
+
+            Console.WriteLine(PI);
         }
     }
 }
